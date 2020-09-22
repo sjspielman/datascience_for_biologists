89c5f5f392eea8cc36169cf4c7fb5e2155f7d20f
Stephanie J. Spielman <spielman@rowan.edu>
2020-09-22 12:42:14 -0400

    HW3

---
+++ fall2020/assignments/hw3/hw3.R
@@ -0,0 +1,68 @@
+##########################################################################################
+## Homework 3
+## Due THURSDAY 10/1/20 by 12:01 PM (noon) to Canvas 
+## Provide your code AND associated comments in the space below each question.
+## Do NOT delete the questions! Please keep formatting as is. 
+## All figures you make MUST BE saved to a variable and printed using the `print()` function. The thing to print is the saved plot VARIABLE itself!
+## This document must run with "Source" without errors or bugs for full credit. A 20% deduction is automatically applied to code with errors.
+
+## FULL INSTRUCTIONS ARE ON THE CLASS WEBSITE
+##########################################################################################
+
+## Who are you -----------------------------------------------------------------
+
+## Your name: 
+## Names of friends you worked with:
+# name here
+# another name here
+# maybe you did the hw alone, which is totally fine, so no need for names here!
+
+## Load the ggplot2 library ----------------------------------------------------
+# Remember: You need to run this line once in each R session so R knows about ggplot!
+library(ggplot2)
+
+
+
+## Code to re-create Plot 1 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 2 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 3 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 4 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 5 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 6 ------------------------------------------------------------
+
+
+
+
+
+## Code to re-create Plot 7 ------------------------------------------------------------
+
+
+
+
+
+## [OPTIONAL BUT HIGHLY ENCOURAGED] Code to re-create Bonus Plot -----------------------
