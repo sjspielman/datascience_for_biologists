1bd1e2412c64ad584986a300f87a85ffd315876b
Stephanie J. Spielman <spielman@rowan.edu>
2020-11-04 18:05:37 -0500

    TOWARDS updating lm tutorial and writing lm homework

---
+++ docs/fall2020/assignments/hw9/hw9.R
@@ -0,0 +1,131 @@
+##########################################################################################
+## Homework 9
+## Due Thursday 11/12/20 by 12:01 PM (noon) to Canvas 
+## Provide your code AND associated comments in the space below each question.
+## Do NOT delete the questions! Please keep formatting as is. 
+## This document must run with "Source" without errors or bugs for full credit. 
+
+## FULL INSTRUCTIONS ARE ON THE CLASS WEBSITE
+##########################################################################################
+
+## Who are you -----------------------------------------------------------------
+
+## Your name: 
+
+
+
+## Load libraries ------------------------------------------------------
+library(tidyverse) 
+
+## Code to read in the dataset --------------------------------------------
+
+
+## Part 0 -----------------------------------------------------------------
+
+
+## Part 1 -----------------------------------------------------------------
+
+### Check and interpret linearity assumption
+
+
+
+### Build model
+
+
+### Check residuals normal assumption
+
+
+### Interpret:
+
+# Interpret the intercept on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH INTERCEPT INTERPRETATION
+
+# Interpret the slope (balance coefficient) on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH SLOPE INTERPRETATION
+
+# Interpret the R^2 value on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH R^2 INTERPRETATION
+
+# Conclude whether balance is a strong or weak predictor of aroma, considering both "biological significance" (effect size!) as well as statistical significance.
+# REPLACE THIS COMMENT WITH OVERALL MODEL CONCLUSION
+
+### Visualize model result
+
+
+## Part 2 -----------------------------------------------------------------
+
+### Subset data to the question scope 
+
+
+
+### Check and interpret equal variance assumption
+
+
+
+### Build model
+
+
+### Check residuals normal assumption
+
+
+### Interpret:
+
+# Interpret the intercept on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH INTERCEPT INTERPRETATION
+
+# Interpret all country coefficients on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH INTERPRETATIONS OF COEFFICIENTS:
+
+# Interpret the R^2 value on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH R^2 INTERPRETATION:
+
+# Conclude whether country is a strong or weak predictor of aroma, considering both "biological significance" (effect size!) as well as statistical significance.
+# REPLACE THIS COMMENT WITH OVERALL MODEL CONCLUSION
+
+
+### Visualize model result
+
+
+## Part 3 -----------------------------------------------------------------
+
+### Build BOTH models
+
+
+### Check residuals normal assumption for BOTH models
+
+
+
+### Interpret:
+
+# Which model is preferred: model with or without interaction? Clearly explain below.
+# REPLACE THIS COMMENT WITH YOUR CONCLUSION FOR WHICH MODEL IS PREFERRED
+
+# Considering the PREFERRED model, Interpret the R^2 value on the next line(s) as comments:
+# REPLACE THIS COMMENT WITH R^2 INTERPRETATION:
+
+# Considering the PREFERRED model, whether country and balance provide a strong or weak predictor of aroma, considering both "biological significance" (effect size!) as well as statistical significance.
+# REPLACE THIS COMMENT WITH OVERALL MODEL CONCLUSION
+
+
+### Visualize model result
+
+
+
+## Part 4 -----------------------------------------------------------------
+
+## Predict:
+
+
+
+## Answer:
+
+
+## Bonus part -----------------------------------------------------------------
+
+# Model selection:
+
+
+# Interpretation:
+# on this/next line(s) 
+
+
