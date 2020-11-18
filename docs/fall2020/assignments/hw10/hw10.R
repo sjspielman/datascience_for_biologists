3a8a5c0ace6ec8a702a63b43eab0a6fe7a36fd39
Stephanie J. Spielman <spielman@rowan.edu>
2020-11-18 17:58:25 -0500

    hw10

---
+++ docs/fall2020/assignments/hw10/hw10.R
@@ -0,0 +1,54 @@
+##########################################################################################
+## Homework 10
+## Due FRIDAY 11/27/20 by 11:59 PM to Canvas 
+## Provide your code AND associated comments in the space below each question.
+## Do NOT delete the questions! Please keep formatting as is. 
+## This document must run with "Source" without errors or bugs for full credit. 
+
+## FULL INSTRUCTIONS ARE ON THE CLASS WEBSITE
+##########################################################################################
+
+## Who are you -----------------------------------------------------------------
+
+## Your name: 
+
+
+
+## Load libraries ------------------------------------------------------
+library(tidyverse) 
+library(pROC)
+
+## Code to read in the dataset --------------------------------------------
+
+
+
+## Part 1 -----------------------------------------------------------------
+
+
+
+# Answer:
+# replace this text with your full answer.
+
+## Part 2 -----------------------------------------------------------------
+
+
+
+
+## Part 3 -----------------------------------------------------------------
+
+
+
+
+## Part 4 -----------------------------------------------------------------
+
+# First, create the tibble with the total number of TP, FP, TN, and FN in the data:
+
+
+# Calculate accuracy here:
+
+# Calculate the false positive rate here:
+
+# Calculate the true positive rate here:
+
+# Calculate the positive predictive value here:
+
