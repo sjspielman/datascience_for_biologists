2b6fb9699f2b2309d6016a40e8d3498cd6956e4f
Stephanie J. Spielman <spielman@rowan.edu>
2021-01-24 16:46:32 -0500

    Updated template and instructions

--- docs/homeworks/hw4/hw4_template.R
+++ docs/homeworks/hw4/hw4_template.R
@@ -2,7 +2,17 @@
 
 ## Your name: REPLACE LITERALLY THIS TEXT WITH YOUR NAME
 
+## Load library --------------------------------------
+library(tidyverse)  # lets you use ggplot2 and forcats (among other things we will learn later!)
 
+## Load the data --------------------------------------
+
+# This code will define the data frame sparrows for you to use for plotting:
+sparrows <- readr::read_csv("data/sparrows.csv", 
+  col_types = list(Age = col_factor(),
+                   Survival = col_factor(),
+                   Sex = col_factor() ))
+                   
 
 
 ## Recreate Plot 1 ----------------------------------------
