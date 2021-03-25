39839a22654da720c6651f100aa3ccea751a9cf4
Stephanie J. Spielman <spielman@rowan.edu>
2021-03-25 13:59:04 -0400

    code

--- docs/homeworks/hw9/clean_and_join_chunk_code.R
+++ docs/homeworks/hw9/clean_and_join_chunk_code.R
@@ -6,12 +6,27 @@ crops %>%
 gender %>%
   filter(County != "Total") %>%
   mutate(County = if_else(County == "Nairobi City", 
-                          "Nairobi", 
-                          County)) -> gender_clean_county
+                         "Nairobi", 
+                         County)) -> gender_clean_county
 
 households %>%
   filter(County != "Kenya") %>%
   mutate(County = if_else(County == "NairobiCity", 
-                          "Nairobi", 
-                          County)) %>%
+                         "Nairobi", 
+                         County)) %>%
   mutate(County = str_replace(County, "([a-z])([A-Z])", "\\1 \\2")) -> households_clean_county
+
+crops_clean_county %>%
+  pivot_longer(Farming:last_col(),
+               names_to = "farming_type", 
+               values_to = "farming_n_households") %>%
+  replace_na(list(farming_n_households = 0)) -> crops_clean_county_tidy
+
+gender_clean_county %>%
+  select(-Total) %>%
+  pivot_longer(Male:Intersex, 
+               names_to = "sex", 
+               values_to = "quantity_sex") -> gender_clean_county_tidy
+
+full_join(households_clean_county, crops_clean_county_tidy) %>%
+  full_join(gender_clean_county_tidy) -> final_kenya
