8599a10abd60fca1c161b78680b122163d07bbff
Stephanie J. Spielman <spielman@rowan.edu>
2021-08-25 15:15:30 -0400

    misc

---
+++ docs/homeworks/hw2/hw2_template.R
@@ -0,0 +1,32 @@
+#########################################################################
+#  Template R Script for Homework #2                                    #
+#  Data Science for Biologists, Fall 2021                               #
+#  This completed template is due to Canvas by 1 pm on Tuesday 9/14/21  #
+#########################################################################
+
+
+# Your name:
+
+
+# Answers ----------------------------------
+
+
+# Example Question ------------------------
+print("Example question")
+w <- 10
+print(w)
+
+# Question 1 ------------------------------
+print("Question 1")
+
+# Question 2 ------------------------------
+print("Question 2")
+
+# Question 3 ------------------------------
+print("Question 3")
+
+# Question 4 ------------------------------
+print("Question 4")
+
+# Question 5 ------------------------------
+print("Question 5")
