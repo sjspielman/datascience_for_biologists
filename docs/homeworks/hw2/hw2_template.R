41702552fe1eed3349c5043117c1c9c057ba5cfb
Stephanie J. Spielman <spielman@rowan.edu>
2021-08-25 17:29:54 -0400

    towards hw2'

--- docs/homeworks/hw2/hw2_template.R
+++ docs/homeworks/hw2/hw2_template.R
@@ -13,20 +13,41 @@
 
 # Example Question ------------------------
 print("Example question")
-w <- 10
-print(w)
+
 
 # Question 1 ------------------------------
 print("Question 1")
 
+
 # Question 2 ------------------------------
 print("Question 2")
+
 
 # Question 3 ------------------------------
 print("Question 3")
 
+
 # Question 4 ------------------------------
 print("Question 4")
 
+
 # Question 5 ------------------------------
 print("Question 5")
+# Define two variables:`genus` with the value "Homo" and `species` with the value "sapiens"
+genus <- "Homo"
+species <= "sapiens"
+
+# Now, print the two variables:
+print(genus)
+priint(sapiens)
+
+
+
+# Question 6 ------------------------------
+print("Question 6")
+
+# Use the function `nchar()` ask the Number of CHARacters in the variable `species` which was defined in question5
+num_species_characters <- nchar(species)
+
+# Print the result
+print(num_species_charatcers)
